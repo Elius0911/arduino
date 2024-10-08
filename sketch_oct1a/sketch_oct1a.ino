a4047eb721bf5ed57d0ea5815f3b2f98fb9e6a1b
Elius. <c25887@gmail.com>
2024-10-08 17:36:52 +0800

    blink (1 sec. interval)

--- sketch_oct1a/sketch_oct1a.ino
+++ sketch_oct1a/sketch_oct1a.ino
@@ -20,6 +20,10 @@ int colors[7][3] = {
 String colorNames[7] = {"red", "yellow", "green", "blue", "cyan", "purple", "white" };
 String currentColorName = colorNames[0];
 
+unsigned long previousMillis = 0;
+const long interval = 1000;  // 閃爍間隔（1秒）
+bool ledOn = true;
+
 void setup() {
   pinMode(ledR, OUTPUT);
   pinMode(ledG, OUTPUT);
@@ -31,8 +35,9 @@ void setup() {
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
+
   btnState = digitalRead(btn);
-  
   if (btnState == HIGH && !btnPressed) {
     // 每次按下按鈕，顏色循環
     ledColor = (ledColor + 1) % 7;
@@ -42,10 +47,23 @@ void loop() {
   else if (btnState == LOW && btnPressed) {
     btnPressed = false;
   }
-  
-  digitalWrite(ledR, colors[ledColor][0] ? LOW : HIGH);
-  digitalWrite(ledG, colors[ledColor][1] ? LOW : HIGH);
-  digitalWrite(ledB, colors[ledColor][2] ? LOW : HIGH);
-  Serial.print("Current Color: ");
-  Serial.println(currentColorName);
+
+  // 使用 millis() 來控制閃爍
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis; // 記錄當前時間
+    ledOn = !ledOn; // 反轉LED狀態
+
+    if (ledOn) {
+      digitalWrite(ledR, colors[ledColor][0] ? LOW : HIGH);
+      digitalWrite(ledG, colors[ledColor][1] ? LOW : HIGH);
+      digitalWrite(ledB, colors[ledColor][2] ? LOW : HIGH);
+    } else {
+      digitalWrite(ledR, HIGH);
+      digitalWrite(ledG, HIGH);
+      digitalWrite(ledB, HIGH);
+    }
+
+    Serial.print("Current Color: ");
+    Serial.println(currentColorName);
+  }
 }
